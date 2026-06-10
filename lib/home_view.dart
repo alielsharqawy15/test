c0fabe5e6c24fa42ca9a27672f5f3fb6a03cf3bd
alielsharqawy <alielsharqawy250@gmail.com>
2026-06-10 09:44:45 +0400

    -- add new commit ali home

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -5,6 +5,13 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home')));
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(child: Text('Home')),
+          Text('Home Ali'),
+        ],
+      ),
+    );
   }
 }
